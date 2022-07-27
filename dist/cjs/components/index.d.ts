119d8223abfd47aa7de6c77846419f54097e5eaa
Animesh Priyadarshi <animeshpriyadarshi@Animeshs-MacBook-Pro.local>
2022-07-27 20:58:12 +0900

    rem dist

--- dist/cjs/components/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { HeaderBar } from "./HeaderBar";
-import { HeaderButton } from "./HeaderButton";
-import { HeaderTabs } from "./HeaderTabs";
-import { TopNavLogo } from "./TopNavLogo";
-export { HeaderBar, HeaderButton, HeaderTabs, TopNavLogo };
